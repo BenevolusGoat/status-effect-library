8f9333cee5457d4c6d7cb5b0ac3ec40147f736a3
BenevolusGoat <brude602@gmail.com>
2025-01-26 21:35:43 -0500

    The actual library + working example

---
+++ main.lua
@@ -0,0 +1,25 @@
+---@class ModReference
+_G.SELExample = RegisterMod("Status Effect Library Example", 1)
+include("src_statuseffectlibrary.status_effect_library")
+local SEL = StatusEffectLibrary
+
+local smilyFace = Sprite()
+smilyFace:Load("gfx/status_effect_smiley_face.anm2", true)
+smilyFace:Play("Idle", true)
+
+SEL.RegisterStatusEffect(
+	"SMILY_FACE",
+	smilyFace
+)
+
+local function happyTime(_, npc)
+	SEL:AddStatusEffect(
+		npc,
+		SEL.StatusFlag.SMILY_FACE,
+		-1,
+		EntityRef(npc),
+		nil
+	)
+end
+
+SELExample:AddCallback(ModCallbacks.MC_POST_NPC_INIT, happyTime)
